1d36b00dbea5e7eeceda228a2c27a950cec7cdeb
seh4835 <sehersanghani5@gmail.com>
2026-02-10 08:43:02 +0530

    merge all files

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #Calculator 
+
+import addition as a
+import subtraction as s
+import multiplication as m
+
+print("Addition result= ", a.add(5,6))
+print("Subtraction result= ", s.sub(5,6))
+print("Multiplication result= ", m.mul(5,6))
